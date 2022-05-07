8eb37b33c45d3d6664195295a3ceb238e553e045
DVDFE <davidfe.pro@gmail.com>
2022-05-07 13:50:23 +0200

    first commit

---
+++ front/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import LoginVue from "./components/login.vue"
+import Navbar from "./components/Navbar.vue"
+
+export default{
+    name:"App",
+    components: {
+        LoginVue,
+        Navbar,
+        Navbar
+    }
+}
+</script>
+
+<template>
+<navbar />
+<router-view></router-view>
+</template>
+
+<style>
+</style>
